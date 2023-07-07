bcc181e7892dbf471c9aa8bf954095fe0d141e50
Matheus Fernando Finatto <78125792+MatheusFinatto@users.noreply.github.com>
2023-07-07 14:00:01 -0300

    skeleton model ideia

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -13,14 +13,39 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
-        static int[,] valores = new int[80, 2];
-        static int[] resultados = new int[80];
+
+        static readonly int QTD_CASOS = 80;
+        static int[,] valores = new int[QTD_CASOS, 2];
+        static int[] resultados = new int[QTD_CASOS];
+        int[] qtdThreads = { 1, 2, 4, 8, 16 };
         static int endThreads = 0;
+        static string actualProblem = "";
+        static int presenteIteração = 0;
+        static int qtdDeCasosInicial = 0;
+        static int qtdDeCasosFinal = 0;
 
-       
-        private void Hashmat_Click(object sender, EventArgs e)
+        void Thread()
         {
+            // a cada thread iniciada, atualiza a qtd final para que compute diferentes dados
+            qtdDeCasosFinal = QTD_CASOS / qtdThreads[presenteIteração] + qtdDeCasosInicial;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("iteração {0}, qtdDeCasosInicial {2}, qtdDeCasosFinal {1}", presenteIteração, qtdDeCasosFinal, qtdDeCasosInicial);
 
+            for (int i = qtdDeCasosInicial; i < qtdDeCasosFinal; i++)
+            {
+                //aqui irão as funções de resolução do problema;
+                if (actualProblem == "hashmat") ResolveHashmat();
+                Console.WriteLine("oi {0}", i);
+            }
+
+            // a quantidade inicial deve ser atualizada depois do for e não antes, para que a primeira iteração ocorra com o qtdDeCasosInicial = 0
+            qtdDeCasosInicial += QTD_CASOS / qtdThreads[presenteIteração];
+            endThreads++;
+        }
+
+        private void MainFunction()
+        {
             // Clear existing series data in the chart
             timeComparissonChart.Series.Clear();
 
@@ -40,74 +65,65 @@ namespace WindowsFormsApp1
             timeComparissonChart.Titles.Add("Comparison of Elapsed Time");
             timeComparissonChart.ChartAreas[0].AxisX.Title = "Thread Count";
             timeComparissonChart.ChartAreas[0].AxisY.Title = "Elapsed Time (ms)";
-            timeComparissonChart.ChartAreas[0].AxisY.Maximum = 100;
-
-
-            Thread[] threads = new Thread[2];
-            endThreads = 0;
+            //timeComparissonChart.ChartAreas[0].AxisY.Maximum = 100;
 
             // Populate valores array
             Random random = new Random();
-            for (int i = 0; i < 80; i++)
+            for (int i = 0; i < QTD_CASOS; i++)
             {
-                for (int j = 0; j < 2; j++) 
+                for (int j = 0; j < 2; j++)
                     valores[i, j] = random.Next(10);
             }
 
-
-
-            threads[0] = new Thread(() =>
-            {
-                for (int i = 0; i < 40; i++)
-                {
-                    resultados[i] = 0;
-                    
-                    if (valores[i, 0] > valores[i, 1]) Console.WriteLine($"THREAD 1: Resposta {i}: VITÓRIA");
-                    else if ((valores[i, 0] == valores[i, 1]))  Console.WriteLine($"THREAD 1: Resposta {i}: EMPATE");
-                    else Console.WriteLine($"THREAD 1: Resposta {i}: DERROTA");
-                   
-                }
-
-                    endThreads++;
-                
-            });
-
-            threads[1] = new Thread(() =>
-            {
-                for (int i = 40; i < 80; i++)
-                {
-                    resultados[i] = 1;
-                    
-                    if (valores[i, 0] > valores[i, 1]) Console.WriteLine($"THREAD 2: Resposta {i}: VITÓRIA");
-                    else if ((valores[i, 0] == valores[i, 1]))  Console.WriteLine($"THREAD 2: Resposta {i}: EMPATE");
-                    else Console.WriteLine($"THREAD 2: Resposta {i}: DERROTA");
-                    
-                }
-                    endThreads++;
-            });
-
             Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-            threads[0].Start();
-            threads[1].Start();
 
-            while (true)
+            //inicia o loop principal (numero de testes feitos)
+            for (int i = 0; i < qtdThreads.Length; i++)
             {
 
-                if (endThreads >= 2)
+                //cria um array de threads que irá receber as instâncias
+                Thread[] threadsExecutaveis = new Thread[qtdThreads[i]];
+                endThreads = 0;
+                presenteIteração = i;
+                qtdDeCasosInicial = 0;
+                qtdDeCasosFinal = 0;
+
+                stopwatch.Start();
+
+                for (int j = 0; j < qtdThreads[i]; j++)
                 {
-                    break;
+                    //instancia a qtd de threads necessarias para a presente iteração 
+                    //se for a primeira, instancia 1 thread, se for a ultima, instancia 16 threads, etc;
+                    threadsExecutaveis[j] = new Thread(Thread);
+                    threadsExecutaveis[j].Start();
                 }
-                
+
+
+                while (true)
+                {
+                    if (endThreads >= qtdThreads[i])
+                    {
+                        break;
+                    }
+                }
+
+                stopwatch.Stop();
+
+                foreach (Thread thread in threadsExecutaveis) { thread.Interrupt(); }
+
+
+                TimeSpan elapsedTime = stopwatch.Elapsed;
+                timeSeries.Points.AddXY(qtdThreads[i], elapsedTime.TotalMilliseconds);
             }
-            stopwatch.Stop();
-
-
-            //Console.WriteLine(string.Join(", ", resultados));
-            
-
-            TimeSpan elapsedTime = stopwatch.Elapsed;
-            timeSeries.Points.AddXY(2, elapsedTime.TotalMilliseconds);
         }
+
+
+        private void Hashmat_Click(object sender, EventArgs e)
+        {
+            actualProblem = "hashmat";
+            MainFunction();
+        }
+
+        private void ResolveHashmat() { }
     }
 }
